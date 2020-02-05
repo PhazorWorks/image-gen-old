c5b3645a2ba4c20cffb88696fad1402c6107b569
GigaFyde <info@gigafyde.nl>
2020-02-05 18:55:14 +0100

    Nice proof of concept

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const express = require('express')
+const {Canvas} = require('canvas-constructor')
+const fs = require('fs')
 const app = express()
 const port = 3333
-const {Canvas} = require('canvas-constructor')
+const bg = fs.readFileSync('template.jpg')
 
 app.use(express.json())
 
 app.post('/', function (request, response) {
     const canvas = new Canvas(500, 200)
     canvas
+        .addImage(bg, 0,0, 500, 200)
+        .setColor("#FFFFFF")
+        .setShadowBlur(50)
         .addTextFont('assets/Ubuntu-Regular.ttf', 'Ubuntu')
         .setTextFont('10pt Ubuntu')
-        .addText('Added to the queue', 10, 10)
+        .addText('Added to the queue', 10, 15)
         .setTextFont('20pt Ubuntu')
-        .addText(request.body.title, 10, 35, 450)
+        .addText(request.body.title, 10, 40, 450)
         .setTextFont('10pt Ubuntu')
-        .addText('Requested by ' + request.body.author, 10, 180, 450)
+        .addText('Length: ' + calculateLength(request.body.length), 10, 170)
+        .addText('Requested by ' + request.body.author, 10, 190, 450)
     response.type('jpg')
     response.send(canvas.toBuffer())
 })
+
+function calculateLength(millis) {
+    var minutes = Math.floor(millis / 60000)
+    var seconds = ((millis % 60000) / 1000).toFixed(0)
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds
+}
 
 app.listen(port)
