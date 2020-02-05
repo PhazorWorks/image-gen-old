c89d174a6e6f0691ee823c83a864c4fa4a5aaefc
GigaFyde <info@gigafyde.nl>
2020-02-05 17:23:03 +0100

    more work in progress, getting closer :)

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3333
-const { Canvas } = require('canvas-constructor')
+const {Canvas} = require('canvas-constructor')
 
-app.use(express.json());
+app.use(express.json())
 
-app.post('/', function(request, response){
-    const canvas = new Canvas(500,200)
+app.post('/', function (request, response) {
+    const canvas = new Canvas(500, 200)
     canvas
-        .addText(request.body.author, 250, 100)
-    console.log(request.body.type)
+        .addTextFont('assets/Ubuntu-Regular.ttf', 'Ubuntu')
+        .setTextFont('10pt Ubuntu')
+        .addText('Added to the queue', 10, 10)
+        .setTextFont('20pt Ubuntu')
+        .addText(request.body.title, 10, 35, 450)
+        .setTextFont('10pt Ubuntu')
+        .addText('Requested by ' + request.body.author, 10, 180, 450)
     response.type('jpg')
     response.send(canvas.toBuffer())
 })
