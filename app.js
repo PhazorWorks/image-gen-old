92fc6c7cb4f0c78dc6efda4a42523185d419d687
GigaFyde <dev@gigafyde.nl>
2020-02-08 12:20:49 +0100

    Some optimizations thanks to a dear friend :D

--- app.js
+++ app.js
@@ -2,26 +2,24 @@ const express = require('express')
 const {Canvas} = require('canvas-constructor')
 const fs = require('fs')
 const request = require('request')
-const util = require('util')
 const app = express()
 const port = 3003
 const bg = fs.readFileSync('template.png')
 
-let download = function (uri, filename, callback) {
-    request.head(uri, function (err, res, body) {
+function download(uri, filename, callback) {
+    request.head(uri, function () {
         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback)
     })
 }
 
 app.use(express.json())
+
 app.post('/', function (req, res) {
     const canvas = new Canvas(500, 169)
+
     download(req.body.url, 'file.png', function () {
-        if (req.body.url.includes("youtube.com")) {
-            canvas.addImage(fs.readFileSync('file.png'), 200, -28, 300, 225,)
-        } else {
-            canvas.addImage(fs.readFileSync('file.png'), 200, 0, 300, 169,)
-        }
+        if (req.body.url.includes("youtube.com")) canvas.addImage(fs.readFileSync('file.png'), 200, -28, 300, 225,)
+        else canvas.addImage(fs.readFileSync('file.png'), 200, 0, 300, 169,)
         canvas
             .addImage(bg, 0, 0, 400, 169)
             .setColor("#FFFFFF")
@@ -33,6 +31,7 @@ app.post('/', function (req, res) {
             .setTextFont('10pt Ubuntu')
             .addText('Length: ' + calculateLength(req.body.length), 10, 139)
             .addText('Requested by ' + req.body.author, 10, 159, 450)
+
         res.type('png')
         res.send(canvas.toBuffer())
 
