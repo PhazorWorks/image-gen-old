0fb29b29e9703eda853bcf2add6c321e2ad337ac
GigaFyde <info@gigafyde.nl>
2020-02-24 20:06:36 +0100

    bunch of changes, and japanese chars support?

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM node:12.14.0-buster-slim
 WORKDIR /app
-COPY app.js app.js
-COPY assets/ assets/
+
+RUN apt update -y
+RUN apt install fontconfig -y
+RUN fc-cache -fv
+
+COPY assets/images assets/images
+COPY assets/fonts /usr/local/share/fonts/
 COPY package.json package.json
 COPY package-lock.json package-lock.json
 RUN npm ci
+
+COPY app.js app.js
+
 EXPOSE 3003
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -28,13 +28,13 @@ app.post('/convert', async function (req, res) {
             canvas.addImage(fs.readFileSync('file.png'), 600, -125, 1300, 965,)
                 .addImage(fade, 0, 0, 1800, 720)
                 .setColor('white')
-                .setTextFont('35pt Utopia')
+                .setTextSize(35)
                 .addText('Added to the queue', 10, 45)
-                .setTextFont('42pt Utopia')
+                .setTextSize(42)
                 .addWrappedText(req.body.title, 10, 120, 580)
-                .setTextFont('35pt Utopia')
+                .setTextSize(35)
                 .addText('Length: ' + calcLength(req.body.duration), 10, 650)
-                .addText('Requested by ' + req.body.author, 10, 700)
+                .addText('Requested by ' + req.body.author, 10, 700, 450)
             res.type('png')
             res.send(canvas.toBuffer())
             try {
@@ -46,12 +46,12 @@ app.post('/convert', async function (req, res) {
         return
     } else canvas.addImage(bg, 0, 0, 1920, 720,)
         .setColor('white')
-        .setTextFont('35pt Utopia')
+        .setTextSize(35)
         .setTextAlign("center")
         .addWrappedText('Added to the queue', 960, 45, 1850)
-        .setTextFont('55pt Utopia')
+        .setTextSize(55)
         .addWrappedText(req.body.title, 960, 250, 1850)
-        .setTextFont('35pt Utopia')
+        .setTextSize(35)
         .setTextAlign("right")
         .addWrappedText('Length: ' + calcLength(req.body.duration), 900, 700, 500)
         .setTextAlign("left")
@@ -64,7 +64,7 @@ app.post('/np', async function (req, res) {
     const canvas = new Canvas(1920, 720)
         .addImage(bg, 0, 0, 1920, 720)
         .setColor("white")
-        .setTextFont('55pt Utopia')
+        .setTextSize(55)
         .setTextAlign("center")
         .addWrappedText(req.body.title, 960, 200, 1850)
         .addText(calcLength(req.body.position) + "/" + calcLength(req.body.duration), 960, 400, 1850)
@@ -92,5 +92,10 @@ function calcLength(length) {
     let seconds = ((length % 60000) / 1000).toFixed(0)
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds
 }
+
+app.use(function onError(err, req, res) {
+    res.statusCode = 500
+    res.end(res.sentry + '\n')
+})
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
